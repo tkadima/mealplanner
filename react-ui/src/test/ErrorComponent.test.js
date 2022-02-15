d441d00967c9035102f49bcab5d2dfb3299f75d8
tkadima <tabithakadima@gmail.com>
2022-02-14 22:19:37 -0500

    Rename client folder in package.json

--- react-ui/src/test/ErrorComponent.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import ErrorComponent from '../components/ErrorComponent'
-
-test('should test ErrorComponent component', () => {
-    const wrapper = shallow(<ErrorComponent />);
-    expect(wrapper).toMatchSnapshot();
-   });
--- react-ui/src/test/Layout.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Layout from '../components/Layout'
-
-test('should test Layout component', () => {
-    const wrapper = shallow(<Layout />);
-    expect(wrapper).toMatchSnapshot();
-   });
