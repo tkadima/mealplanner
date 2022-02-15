285c9d68fcd987c865f997b105b19f5120915b97
tkadima <tabithakadima@gmail.com>
2022-02-14 22:55:34 -0500

    Add route

--- index.js
+++ index.js
@@ -123,6 +123,14 @@ app.delete('/api/food', (req, res) => {
     )
 })
 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(__dirname + "/client/build"));
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
+  }
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
